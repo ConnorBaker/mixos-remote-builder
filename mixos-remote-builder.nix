5debfbb7852702a64ed1e4f63fee2683cf1bc7c8
Jared Baur <jaredbaur@fastmail.com>
2025-10-26 16:00:24 -0700

    WIP

--- mixos-remote-builder.nix
+++ mixos-remote-builder.nix
@@ -6,7 +6,7 @@
 
   boot.kernel = pkgs.linuxKernel.manualConfig {
     inherit (pkgs.linux_6_12) src version;
-    configfile = ./kernel.config;
+    configfile = ./kernel2.config;
   };
 
   bin = [
